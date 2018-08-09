b04c41b190e1554da2146c03c89544daf7e2f460
michelisondaniel <michelison.daniel@gmail.com>
2018-08-08 19:51:44 -0700

    Changed TextActivity.java to BotActivity.java and changed activty_text.xml to activity_bot.xml, also have been working on the JSONHandler, which will not refactor to CSVHandler.

--- app/src/main/java/net/michelison/homebitbot/BotActivity.java
+++ app/src/main/java/net/michelison/homebitbot/BotActivity.java
@@ -27,7 +27,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 
-public class TextActivity extends Activity {
+public class BotActivity extends Activity {
 
     private Button mButtonSend;
     private ListView mListView;
