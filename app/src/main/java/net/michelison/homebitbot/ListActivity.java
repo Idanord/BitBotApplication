b23cc306b7cfd956fb82a2ec27a1cbb7661b2d28
Marianne Goldin <marianne.goldin@gmail.com>
2018-08-13 20:09:24 -0700

    rebuilds branch to where we left off with last decomissioned repo

--- app/src/main/java/net/michelison/homebitbot/ListActivity.java
+++ app/src/main/java/net/michelison/homebitbot/ListActivity.java
@@ -3,15 +3,11 @@ package net.michelison.homebitbot;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class TermsActivity extends Activity {
-
-    private int TermID;
-    private String Term;
-    private String TermDefinition;
+public class ListActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_terms);
+        setContentView(R.layout.activity_list);
     }
 }
